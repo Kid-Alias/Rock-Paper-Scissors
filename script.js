b7128a73a6dd425e4dcee5c7fc20c43762db9076
Kid Alias <166966079+Kid-Alias@users.noreply.github.com>
2026-04-14 20:43:34 -0600

    the html and css are fleshed out for the most part now comes the hard part of figuring javascript out

---
+++ script.js
@@ -0,0 +1,22 @@
+const choices = ['rock','paper','scissors']
+
+const buttons = document.querySelectorAll('.game-btn')
+
+buttons = 
+
+const emojis = {
+    rock:     '✊',
+    paper:    '✋',
+    scissors: '✌️'
+}
+
+function updateArena(playerChoice, computerChoice) {
+
+    const playerChoice
+}
+
+let scores = {
+    user: 0,
+    computer: 0,
+    tie: 0,
+};
